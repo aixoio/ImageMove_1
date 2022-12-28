2219fd62619b1f5134018009ee4094ae729bc858
aixoio <aixoio@yahoo.com>
2022-12-28 15:30:38 +1100

    Add base files for objects

---
+++ src/objects/Drawable.java
@@ -0,0 +1,9 @@
+package objects;
+
+import java.awt.Graphics2D;
+
+public interface Drawable {
+	
+	abstract public void draw(Graphics2D g2d);
+
+}
---
+++ src/objects/Updatable.java
@@ -0,0 +1,7 @@
+package objects;
+
+public interface Updatable {
+	
+	abstract public void update();
+
+}
