bee73f4d37a300a264100d08aa13c37424171df5
aixoio <aixoio@yahoo.com>
2022-12-28 15:53:01 +1100

    Add xv and yv to BaseObject

--- src/objects/BaseObject.java
+++ src/objects/BaseObject.java
@@ -5,14 +5,21 @@ import java.awt.Color;
 import objects.base.GameObject;
 
 public class BaseObject extends GameObject {
+	
+	private float xv;
+	private float yv;
 
 	public BaseObject(float x, float y, float width, float height, Color color) {
 		super(x, y, width, height, color);
+		this.xv = 0;
+		this.yv = 0;
 	}
 
 	@Override
 	public void update() {
 		
+		this.x += this.xv;
+		this.y += this.yv;
 		
 		
 	}
